cdb77108b4282cf385113d2cdaca213a07d328af
Alex York <alexyork1985@gmail.com>
2026-08-18 22:32:24 +0200

    Improved docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,15 +14,6 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'BrainBase', // Usually your GitHub org/user name.
-  projectName: 'brainbase-wiki', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  
   presets: [
     [
       'classic',
@@ -30,14 +21,12 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/BrainBaseNo/brainbase-wiki/tree/main/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/BrainBaseNo/brainbase-wiki/tree/main/',
